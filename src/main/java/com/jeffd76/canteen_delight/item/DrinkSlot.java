35a8b2b276aecfe0282734d1e9f7dc37a6a1591a
JeffD76 <jeff.s.demarest@gmail.com>
2026-04-01 15:57:23 -0600

    Initial commit

---
+++ src/main/java/com/jeffd76/canteen_delight/item/DrinkSlot.java
@@ -0,0 +1,17 @@
+package com.jeffd76.canteen_delight.item;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
+import net.neoforged.neoforge.items.IItemHandler;
+import net.neoforged.neoforge.items.ItemHandlerCopySlot;
+
+public class DrinkSlot extends ItemHandlerCopySlot {
+    public DrinkSlot(IItemHandler handler, int index, int x, int y) {
+        super(handler, index, x, y);
+    }
+
+    @Override
+    public boolean mayPlace(ItemStack stack) {
+        return stack.getUseAnimation() == UseAnim.DRINK && super.mayPlace(stack);
+    }
+}
---
+++ src/main/java/com/jeffd76/canteen_delight/tooltip/CanteenTooltip.java
@@ -0,0 +1,9 @@
+package com.jeffd76.canteen_delight.tooltip;
+
+import net.minecraft.world.inventory.tooltip.TooltipComponent;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
+
+public record CanteenTooltip(List<ItemStack> items, int slotCount) implements TooltipComponent {
+}
